64abc9c3b85ddb472315f97130f7cfd13211bc8d
Wei Wang <ww@9rivers.com>
2022-08-05 00:16:50 -0400

    Reset logo dimention

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="150" height="58" />
 
     <div class="wrapper">
       <HelloWorld msg="Welcome!" />
